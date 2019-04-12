a4afb2885511ddeb2d25d8adbfa8dc269878ffc2
Anwesh43 <anweshthecool0@gmail.com>
2019-04-12 11:40:53 +0530

    adding RotThenFloor class

--- rotthenfloorview/src/main/java/com/anwesh/uiprojects/rotthenfloorview/RotThenFloorView.kt
+++ rotthenfloorview/src/main/java/com/anwesh/uiprojects/rotthenfloorview/RotThenFloorView.kt
@@ -166,4 +166,28 @@ class RotTheFloorView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RotThenFloor(var i : Int) {
+
+        private val root : RTFNode = RTFNode(0)
+        private var curr : RTFNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
