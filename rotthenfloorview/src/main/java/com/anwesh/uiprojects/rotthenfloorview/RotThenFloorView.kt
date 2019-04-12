a825e6ea3ecc36e700d3b153c65d6b00948cb34f
Anwesh43 <anweshthecool0@gmail.com>
2019-04-12 11:38:55 +0530

    adding RTFNode class

--- rotthenfloorview/src/main/java/com/anwesh/uiprojects/rotthenfloorview/RotThenFloorView.kt
+++ rotthenfloorview/src/main/java/com/anwesh/uiprojects/rotthenfloorview/RotThenFloorView.kt
@@ -122,4 +122,48 @@ class RotTheFloorView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RTFNode(var i : Int, val state : State = State()) {
+
+        private var next : RTFNode? = null
+        private var prev : RTFNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = RTFNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRTFNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RTFNode {
+            var curr : RTFNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
