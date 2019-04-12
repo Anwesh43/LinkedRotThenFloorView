215db0e2afd182c79d8377ac0f10f0f2d1a60475
Anwesh43 <anweshthecool0@gmail.com>
2019-04-12 11:46:32 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrotthenfloorview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrotthenfloorview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.linkedrotthenfloorview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.rotthenfloorview.RotThenFloorView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        RotThenFloorView.create(this)
     }
 }
