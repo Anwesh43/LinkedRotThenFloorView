084319d945b393c8643db8ac3b7903b83c300692
Anwesh43 <anweshthecool0@gmail.com>
2019-04-12 11:27:52 +0530

    adding drawRTFNode canvas extension method

--- rotthenfloorview/src/main/java/com/anwesh/uiprojects/rotthenfloorview/RotThenFloorView.kt
+++ rotthenfloorview/src/main/java/com/anwesh/uiprojects/rotthenfloorview/RotThenFloorView.kt
@@ -29,3 +29,31 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
 fun Float.updateTo(dx : Float, sc : Float) : Float = this + (dx - this) * sc
+
+fun Canvas.drawRotThenFloorLine(i : Int, sc1 : Float, sc2 : Float, size : Float, paint : Paint) {
+    val yGap : Float = (2 * size) / lines
+    val y : Float = (yGap * i).updateTo(2 * size, sc2)
+    save()
+    translate(0f, -size + y + yGap)
+    rotate(angleDeg * sc1 * -1)
+    drawLine(0f, 0f, 0f, -yGap, paint)
+    restore()
+}
+
+fun Canvas.drawRTFNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, lines)
+    val sc2 : Float = scale.divideScale(1, lines)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND 
+    save()
+    translate(w / 2, gap * (i + 1))
+    for (j in 0..(lines - 1)) {
+        drawRotThenFloorLine(j, sc1, sc2, size, paint)
+    }
+    restore()
+}
